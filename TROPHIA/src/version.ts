a9a35ab6c0f0aadd82da51d44744d5ff3eb1bd56
Indigl0 <felipeibarra@MacBook-Pro-de-Felipe.local>
2026-08-31 21:36:15 -0400

    Actualiza a versión 1.2.0 y añade mejoras

--- TROPHIA/src/version.ts
+++ TROPHIA/src/version.ts
@@ -1,2 +1,2 @@
 // src/version.ts
-export const APP_VERSION = "v1.1.0"; // Puedes actualizar este número cada vez que hagas cambios importantes
+export const APP_VERSION = "v1.2.0"; // Puedes actualizar este número cada vez que hagas cambios importantes
